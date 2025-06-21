acee337f5da476330c182930d1a6943d972494d1
Yerinnnn <shinyerin0216@gmail.com>
2025-06-21 19:25:33 +0900

    feat: new_rbtree 구현

--- src/rbtree.c
+++ src/rbtree.c
@@ -3,8 +3,25 @@
 #include <stdlib.h>
 
 rbtree *new_rbtree(void) {
+  // 트리 구조체 메모리 할당
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
+
+  // nil 노드 생성
+  node_t *nil_node = (node_t *)calloc(1, sizeof(node_t));
+  nil_node->color = RBTREE_BLACK;  // nil은 항상 black
+  // nil_node->left = nil_node->right = nil_node->parent = NULL;  // 명시적으로 적어주는 게 좋을까, 아닐까?
+
+  // 성종 오빠가 추가한 부분
+  nil_node->key = 0;  // 오빠는 malloc을 사용했고, 그럴 때는 이 멤버를 사용하지 않아도 꼭 초기화 해주는 편임 (malloc 할 때는 모든 멤버를 꼭 초기화 해주는 게 좋음 -> 어떤 에러가 발생할지 모르니까)
+  nil_node->left = nil_node;
+  nil_node->right = nil_node;
+  nil_node->parent = nil_node;
+
+  // 트리 설정
+  p->nil = nil_node;
+  p->root = nil_node;
+
   return p;
 }
 
